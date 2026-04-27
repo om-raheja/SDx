9463aaa31cf39259004b767a6176223dd008d74b
Om Raheja <rahejaom@outlook.com>
2026-04-27 04:32:24 +0000

    Fix access type

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
 
     const buffer = Buffer.from(await file.arrayBuffer());
     const blob = await put(file.name, buffer, {
-      access: 'public',
+      access: 'private',
       token: process.env.BLOB_READ_WRITE_TOKEN,
     });
 
